ae61eaf16f0a7fff289b67cae2962ea77f330e5d
belkan4 <abjelkic4@gmail.com>
2024-03-07 19:25:19 +0100

    vezba2

---
+++ OTS2023_ConventorApp/ConvertLength.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTS2023_ConventorApp
+{
+    public class ConvertLength : IConvert
+    {
+        public double Convert(double value1)
+        {
+            return value1 * 0.304;
+        }
+    }
+}
--- OTS2023_ConventorApp/Form1.cs
+++ OTS2023_ConventorApp/Form1.cs
@@ -16,5 +16,39 @@ namespace OTS2023_ConventorApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                if (textBox1.Text != null)
+                {
+                    double n = Convert.ToInt32(textBox1.Text);
+                    double result = new ConvertMass().Convert(n);
+                    textBox2.Text = result.ToString();
+                }
+            }
+            else if (radioButton2.Checked)
+            {
+                if (textBox1.Text != null)
+                {
+                    double n = Convert.ToInt32(textBox1.Text);
+                    double result = new ConvertLength().Convert(n);
+                    textBox2.Text = result.ToString();
+                }
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "pounds";
+            label2.Text = "kg";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Text = "feet";
+            label2.Text = "m";
+        }
     }
 }
